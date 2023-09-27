ba44a08d38d394a5e22a7953b11a1a3274b42f15
Сергей Савчишкин <rings.of.saturn@yandex.ru>
2023-09-27 21:08:40 +0200

    add dotenv

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -1,24 +1,17 @@
 import os
+from dotenv import load_dotenv
 from pathlib import Path
-from datetime import timedelta
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+load_dotenv()
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECRET_KEY = os.getenv('SECRET_KEY', 'a420a3a748a1497fa8eb244a3f9da85f')
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4asip^omd)@@b&49k3v4gu-(hps(6*r-s-w(0$68^wy3&kbq@i'
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split()
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
